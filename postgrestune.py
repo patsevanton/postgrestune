3b38acfb2fbd380ba13e88501ee33c0713bf2b26
Anton Patsev <apatsev@luxoft.com>
2018-06-05 14:20:30 +0300

    Added check dmesg

--- postgrestune.py
+++ postgrestune.py
@@ -5,14 +5,21 @@
 
 import psutil
 import platform
+import re
+import subprocess
 
 mem = psutil.virtual_memory()
-print( 'OS total memory     : {0} MB'.format(round(mem.total / 1024**2, 0)))
-print( 'system              : {0}'.format(platform.system()))
-print( 'node                : {0}'.format(platform.node()))
-print( 'release             : {0}'.format(platform.release()))
-print( 'version             : {0}'.format(platform.version()))
-print( 'machine             : {0}'.format(platform.machine()))
-print( 'processor           : {0}'.format(platform.processor()))
-print( 'dist                : {0}'.format(platform.dist()))
-print( 'linux_distribution  : {0}'.format(platform.linux_distribution()))
+# print( 'OS total memory     : {0} MB'.format(round(mem.total / 1024**2, 0)))
+# print( 'system              : {0}'.format(platform.system()))
+# print( 'node                : {0}'.format(platform.node()))
+# print( 'release             : {0}'.format(platform.release()))
+# print( 'version             : {0}'.format(platform.version()))
+# print( 'machine             : {0}'.format(platform.machine()))
+# print( 'processor           : {0}'.format(platform.processor()))
+# print( 'dist                : {0}'.format(platform.dist()))
+# print( 'linux_distribution  : {0}'.format(platform.linux_distribution()))
+
+
+test_variable = subprocess.check_output('dmesg',shell=True)
+result = re.findall(r'vmware|kvm|xen|vbox|hyper-v', test_variable)
+print(result)
