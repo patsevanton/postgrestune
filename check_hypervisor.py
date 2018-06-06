4d4228697032b99f196ba24e72f565238e060ee0
Anton Patsev <patsev.anton@gmail.com>
2018-06-06 17:27:32 +0300

    devide postgrestune.py, add check connection

---
+++ check_hypervisor.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+def get_value_proc(path_of_proc):
+    try:
+      with open(path_of_proc, 'r') as f:
+        value_proc = f.readline().split()[0]
+        return(value_proc)
+    except IOError as e:
+        print('ERROR: %s' % e)
+        logging.error("Error {0}".format(e))
+
+def used_sys_block():
+  proc_sys_block = [os.listdir('/sys/block')][0]
+  exclude_sys_block = ('loop', 'sr0')
+  used_sys_block = [x for x in proc_sys_block if not x.startswith(exclude_sys_block)]
+  print('used_sys_block', used_sys_block)
+  return used_sys_block
+
+# https://serverfault.com/questions/693348/what-does-it-mean-when-linux-has-no-i-o-scheduler
+def iter_sys_block_scheduler(self):
+  for iter_sys_block in self.used_sys_block():
+    print('iter_sys_block', iter_sys_block)
+    print('/proc/sys/block/' + iter_sys_block + '/queue/scheduler')
+    iter_sys_block_scheduler = get_value_proc('/sys/block/' + iter_sys_block + '/queue/scheduler')
+    print('iter_sys_block_scheduler', iter_sys_block_scheduler)
+    return iter_sys_block_scheduler
+
+dmesg_output = subprocess.check_output('dmesg',shell=True)
+hypervisor = set(re.findall(r'vmware|kvm|xen|vbox|hyper-v', dmesg_output.decode('utf-8')))
+if len(hypervisor) > 1:
+  logging.error("hypervisor in dmesg more 1")
+elif len(hypervisor) == 1:
+  print("Check scheduler")
+else:
+  print("Running on physical machine")
--- postgrestune.py
+++ postgrestune.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+### Install requirements: ###
 # apt install python3-psutil
 # apt install python3-pip
 # apt-get install python3-psycopg2
@@ -7,6 +8,7 @@
 # pip3 install procfs
 # pip3 install coloredlogs
 
+### PostgreSQL major and minor verion: ###
 POSTGRESQL_VERSION_MAJOR_LATEST='10'
 POSTGRESQL_VERSION_MINOR_LATEST_10='10.4'
 POSTGRESQL_VERSION_MINOR_LATEST_96='9.6.9'
@@ -14,7 +16,8 @@ POSTGRESQL_VERSION_MINOR_LATEST_95='9.5.13'
 POSTGRESQL_VERSION_MINOR_LATEST_94='9.4.18'
 POSTGRESQL_VERSION_MINOR_LATEST_93='9.3.23'
 
-#import psutil
+#### Import modules: ####
+# import psutil
 import platform
 import re
 import subprocess
@@ -40,50 +43,6 @@ logging.getLogger("postgrestune")
 # print( 'dist                : {0}'.format(platform.dist()))
 # print( 'linux_distribution  : {0}'.format(platform.linux_distribution()))
 
-
-
-proc_sys_block = [os.listdir('/sys/block')][0]
-exclude_sys_block = ('loop', 'sr0')
-used_sys_block = [x for x in proc_sys_block if not x.startswith(exclude_sys_block)]
-print('used_sys_block', used_sys_block)
-
-dmesg_output = subprocess.check_output('dmesg',shell=True)
-hypervisor = set(re.findall(r'vmware|kvm|xen|vbox|hyper-v', dmesg_output.decode('utf-8')))
-if len(hypervisor) > 1:
-  logging.error("hypervisor in dmesg more 1")
-elif len(hypervisor) == 1:
-  print("Check scheduler")
-else:
-  print("Running on physical machine")
-
-
-def get_int_proc(path_of_proc):
-    try:
-      with open(path_of_proc, 'r') as f:
-        int_proc = int(f.readline().split()[0])
-        return(int_proc)
-    except IOError as e:
-        print('ERROR: %s' % e)
-        logging.error("Error {0}".format(e))
-
-overcommit_memory = get_int_proc('/proc/sys/vm/overcommit_memory')
-if overcommit_memory != 2:
-  logging.info("overcommit_memory: {0}".format(overcommit_memory))
-  logging.info("""Memory overcommitment is allowed on the system. 
-    This can lead to OOM Killer killing some PostgreSQL process,
-    which will cause a PostgreSQL server restart (crash recovery)""")
-  logging.info("""set vm.overcommit_memory=2 in /etc/sysctl.conf and run sysctl -p to reload it. 
-    This will disable memory overcommitment and avoid postgresql killed by OOM killer.""")
-
-overcommit_ratio = get_int_proc('/proc/sys/vm/overcommit_ratio')
-if overcommit_ratio <= 50:
-  logging.info("overcommit_ratio: {0}".format(overcommit_ratio))
-  logging.info("""vm.overcommit_ratio is too small, 
-    you will not be able to use more than $overcommit_ratio*RAM+SWAP for applications""")
-elif overcommit_ratio >= 90:
-  logging.info("overcommit_ratio: {0}".format(overcommit_ratio))
-  logging.info("vm.overcommit_ratio is too high, you need to keep free space for the kernel")
-
 try:
   conn=psycopg2.connect(
     database="postgres",
@@ -93,49 +52,111 @@ except IOError as e:
   print('ERROR: %s' % e)
   logging.error("Error {0}".format(e))
   # logging.error("I am unable to connect to the database")
-
 cur = conn.cursor()
 
-try:
- cur.execute("SELECT version();")
-except psycopg2.Error as e:
- logging.error("I am unable run query SELECT version();")
+def get_value_proc(path_of_proc):
+    try:
+      with open(path_of_proc, 'r') as f:
+        value_proc = f.readline().split()[0]
+        return(value_proc)
+    except IOError as e:
+        print('ERROR: %s' % e)
+        logging.error("Error {0}".format(e))
 
-postgresql_version=cur.fetchone()[0].split(' ')[1]
-POSTGRESQL_VERSION_MAJOR_CURRENT = re.findall(r'(\d{1,3}\.\d{1,3})', postgresql_version)[0]
-print(postgresql_version)
+def check_overcommit_memory():
+  overcommit_memory = int(get_value_proc('/proc/sys/vm/overcommit_memory'))
+  if overcommit_memory != 2:
+    logging.info("overcommit_memory: {0}".format(overcommit_memory))
+    logging.info("Memory overcommitment is allowed on the system. This can lead to OOM Killer killing some PostgreSQL process, which will cause a PostgreSQL server restart (crash recovery)")
+    logging.info("set vm.overcommit_memory=2 in /etc/sysctl.conf and run sysctl -p to reload it. This will disable memory overcommitment and avoid postgresql killed by OOM killer.")
 
-if version.parse(POSTGRESQL_VERSION_MAJOR_CURRENT) < version.parse(POSTGRESQL_VERSION_MAJOR_LATEST):
-  logging.info("You used not major postgres latest version: {0}".format(POSTGRESQL_VERSION_MAJOR_LATEST))
-  logging.info("You used postgres major version: {0}".format(POSTGRESQL_VERSION_MAJOR_CURRENT))
-  if POSTGRESQL_VERSION_MAJOR_CURRENT == '9.6':
-    if version.parse(postgresql_version) < version.parse(POSTGRESQL_VERSION_MINOR_LATEST_96):
-      logging.error("You used not latest postgres version: {0}".format(POSTGRESQL_VERSION_MAJOR_LATEST))
-  elif POSTGRESQL_VERSION_MAJOR_CURRENT == '9.5':
-    if version.parse(postgresql_version) < version.parse(POSTGRESQL_VERSION_MINOR_LATEST_95):
-      logging.error("You used not latest postgres version: {0}".format(POSTGRESQL_VERSION_MAJOR_LATEST))
-  elif POSTGRESQL_VERSION_MAJOR_CURRENT == '9.4':
-    if version.parse(postgresql_version) < version.parse(POSTGRESQL_VERSION_MINOR_LATEST_94):
-      logging.error("You used not latest postgres version: {0}".format(POSTGRESQL_VERSION_MAJOR_LATEST))
-  elif POSTGRESQL_VERSION_MAJOR_CURRENT == '9.3':
-    if version.parse(postgresql_version) < version.parse(POSTGRESQL_VERSION_MINOR_LATEST_93):
-      logging.error("You used not latest postgres version: {0}".format(POSTGRESQL_VERSION_MAJOR_LATEST))
-else:
-  if version.parse(postgresql_version) < version.parse(POSTGRESQL_VERSION_MINOR_LATEST_10):
-    logging.error("You used not latest postgres version: {0}".format(POSTGRESQL_VERSION_MINOR_LATEST_10))
+check_overcommit_memory()
 
-# logging.debug("This is a debug message")
-# logging.info("Informational message")
-# logging.error("An error has happened!")
+def check_overcommit_ratio():
+  overcommit_ratio = int(get_value_proc('/proc/sys/vm/overcommit_ratio'))
+  if overcommit_ratio <= 50:
+    logging.info("overcommit_ratio: {0}".format(overcommit_ratio))
+    logging.info("vm.overcommit_ratio is too small, you will not be able to use more than $overcommit_ratio*RAM+SWAP for applications")
+  elif overcommit_ratio >= 90:
+    logging.info("overcommit_ratio: {0}".format(overcommit_ratio))
+    logging.info("vm.overcommit_ratio is too high, you need to keep free space for the kernel")
 
-try:
- cur.execute("select usename from pg_shadow where passwd='md5'||md5(usename||usename);")
-except psycopg2.Error as e:
- logging.error("I am unable run query select usename from pg_shadow where passwd='md5'||md5(usename||usename)")
+check_overcommit_ratio()
 
-if cur != None:
-  for k in cur:
-    logging.error("some users account have the username as password : {0}".format(k[0]))
+def check_pg_stat_statements():
+  try:
+   cur.execute("SELECT * FROM pg_available_extensions WHERE name = 'pg_stat_statements' and installed_version is not null;")
+  except psycopg2.Error as e:
+   logging.error("Error {0}".format(e))
+
+  available_pg_stat_statements=cur.fetchone()[0]
+  if available_pg_stat_statements == None:
+    logging.error("Extensions pg_stat_statements is disabled")
+    logging.info("Enable pg_stat_statements to collect statistics on all queries (not only queries longer than log_min_duration_statement in logs)")
+    logging.info("Add the following entries to your postgres.conf: shared_preload_libraries = 'pg_stat_statements'")
+    logging.info("restart the PostgreSQL daemon and run 'create extension pg_stat_statements' on your database")
+
+check_pg_stat_statements()
+
+def max_connections():
+  try:
+   cur.execute("SELECT setting FROM pg_settings WHERE name = 'max_connections';")
+  except psycopg2.Error as e:
+   logging.error("Error {0}".format(e))
+  return int(cur.fetchone()[0])
+
+def current_connections():
+  try:
+   cur.execute("SELECT count(*) FROM pg_stat_activity;")
+  except psycopg2.Error as e:
+   logging.error("Error {0}".format(e))
+  return int(cur.fetchone()[0])
+
+def check_postgresql_version():
+  try:
+   cur.execute("SELECT version();")
+  except psycopg2.Error as e:
+   logging.error("Error {0}".format(e))
+
+  postgresql_version=cur.fetchone()[0].split(' ')[1]
+  POSTGRESQL_VERSION_MAJOR_CURRENT = re.findall(r'(\d{1,3}\.\d{1,3})', postgresql_version)[0]
+  print(postgresql_version)
+
+  if version.parse(POSTGRESQL_VERSION_MAJOR_CURRENT) < version.parse(POSTGRESQL_VERSION_MAJOR_LATEST):
+    logging.info("You used not major postgres latest version: {0}".format(POSTGRESQL_VERSION_MAJOR_LATEST))
+    logging.info("You used postgres major version: {0}".format(POSTGRESQL_VERSION_MAJOR_CURRENT))
+    if POSTGRESQL_VERSION_MAJOR_CURRENT == '9.6':
+      if version.parse(postgresql_version) < version.parse(POSTGRESQL_VERSION_MINOR_LATEST_96):
+        logging.error("You used not latest postgres version: {0}".format(POSTGRESQL_VERSION_MAJOR_LATEST))
+    elif POSTGRESQL_VERSION_MAJOR_CURRENT == '9.5':
+      if version.parse(postgresql_version) < version.parse(POSTGRESQL_VERSION_MINOR_LATEST_95):
+        logging.error("You used not latest postgres version: {0}".format(POSTGRESQL_VERSION_MAJOR_LATEST))
+    elif POSTGRESQL_VERSION_MAJOR_CURRENT == '9.4':
+      if version.parse(postgresql_version) < version.parse(POSTGRESQL_VERSION_MINOR_LATEST_94):
+        logging.error("You used not latest postgres version: {0}".format(POSTGRESQL_VERSION_MAJOR_LATEST))
+    elif POSTGRESQL_VERSION_MAJOR_CURRENT == '9.3':
+      if version.parse(postgresql_version) < version.parse(POSTGRESQL_VERSION_MINOR_LATEST_93):
+        logging.error("You used not latest postgres version: {0}".format(POSTGRESQL_VERSION_MAJOR_LATEST))
+  else:
+    if version.parse(postgresql_version) < version.parse(POSTGRESQL_VERSION_MINOR_LATEST_10):
+      logging.error("You used not latest postgres version: {0}".format(POSTGRESQL_VERSION_MINOR_LATEST_10))
+
+check_postgresql_version()
+
+def check_username_equal_password():
+  try:
+   cur.execute("select usename from pg_shadow where passwd='md5'||md5(usename||usename);")
+  except psycopg2.Error as e:
+   logging.error("Error {0}".format(e))
+
+  if cur != None:
+    for k in cur:
+      logging.error("some users account have the username as password : {0}".format(k[0]))
+
+check_username_equal_password()
+
+current_connections_percent=current_connections()*100/max_connections()
+print(current_connections_percent)
 
 cur.close()
 conn.close()
